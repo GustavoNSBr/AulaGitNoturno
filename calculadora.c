24fd20b573f640ea5869ff846134eb57b98fb04f
GustavoNSBr <gustavonunessilvabr@gmail.com>
2023-08-22 21:02:00 -0300

    Criando calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,8 +1,55 @@
-#include <stdio>
+#include <stdio.h>
+
+// COMENTARIOS DA AULA NO FINAL DO CODIGO
 
 int main(){
+    int valor1;
+    int valor2;
+    int resultado;
+    char operacao;
 
-    printf("Ola")
+    printf("Insira a operacao: \n");
+    scanf("%c", &operacao);
 
-    return 0
+    printf("Insira os valores:\n");
+    printf("Valor 1:\n");
+    scanf("%d", &valor1);
+    printf("Valor 2:\n");
+    scanf("%d", &valor2);
+
+    if(operacao == '+'){
+        resultado = valor1 + valor2;
+    } else if (operacao == '-'){
+        resultado = valor1 - valor2;
+    } else if (operacao == '*'){
+        resultado = valor1 * valor2;
+    } else if (operacao == '/'){
+        resultado = valor1 / valor2;
+    }else if (operacao == '%'){
+        resultado = valor1 % valor2;
+    }
+
+    printf("Resultado %d\n", resultado);
+    printf("operacao %c\n", operacao);
+
+    return 0;
 }
+
+
+
+
+
+
+
+// "+" -> string
+// '+' -> char
+
+
+
+    // Soma: resultado = valor1 + valor2;
+    // Subtração: resultado = valor1 - valor2;
+    // Multiplicão: resultado = valor1 * valor2;
+    // Divisão: resultado = valor1 / valor2;
+    // Resto da Divisao: resultado = valor1 % valor2; // usar pra achar numero primos e restos de divisoes
+    // Incremento: valor1 = valor1 + 1; ou valor1++; // adiciona 1 na variavel
+    // Decremento: valor1 = valor1 - 1; ou valor--; // remove 1 na variavel

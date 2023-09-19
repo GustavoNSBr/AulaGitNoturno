99ce113af356a30fb55664002918fe4853f1bf7b
GustavoNSBr <gustavonunesilvabr@gmail.com>
2023-09-19 20:40:26 -0300

    media_notas_vetor.c

---
+++ Vetores/media_notas_vetor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    double notas[4], somaDasNotas = 0;
+    char nomeAluno[20]; // {'g','u','s','t','a','v','o'}; Feio //
+
+    scanf("%s", &nomeAluno);
+    printf("%s", nomeAluno);
+
+    return 0;
+}
---
+++ Vetores/vetores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int notas[4];
+/*
+    notas[0]= 10;
+    notas[1]= 20;
+    notas[2]= 30;
+    notas[3]= 40;
+
+    printf("%d", notas[2]);
+
+*/
+
+
+    return 0;
+}

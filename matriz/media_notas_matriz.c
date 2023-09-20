f70ae44bed7e6db7489eb51473c9f1a0bf9fa42c
GustavoNSBr <gustavonunesilvabr@gmail.com>
2023-09-19 21:08:59 -0300

    AulaGitNoturno.c

---
+++ matriz/media_notas_matriz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    double notas[2][4];
+
+    for(int aluno = 0; aluno <= 1;aluno++)
+    printf("Insira as notas do aluno %d:\n" aluno + 1);
+           for(posNotas = 0; posNotas < 4;posNotas++){
+        printf("Insira a nota do %d:", i + 1, notas[0][posNotas]);
+        scanf("%lf", &notas[aluno][posNotas]);
+    }
+
+
+    return 0;
+}
